f8546057c2ec87bcd1232f6bc1cba0c55f11b8e6
Muad-Ahmed <muad8010@gmail.com>
2025-11-22 18:40:56 +0300

    feat: Complete NewCommentForm with authorization check

--- resources/js/Components/NewCommentForm.tsx
+++ resources/js/Components/NewCommentForm.tsx
@@ -1,36 +1,43 @@
-import {Feature} from "@/types";
+import { Feature } from "@/types";
 import TextAreaInput from "@/Components/TextAreaInput";
-import {useForm} from "@inertiajs/react";
-import {FormEventHandler} from "react";
+import { useForm, usePage } from "@inertiajs/react";
+import { FormEventHandler } from "react";
 import PrimaryButton from "@/Components/PrimaryButton";
+import { can } from "@/helpers";
 
-export default function NewCommentForm({feature}: { feature: Feature }) {
-  const {
-    data,
-    setData,
-    post,
-    processing
-  } = useForm({
-    comment: ''
-  })
+export default function NewCommentForm({ feature }: { feature: Feature }) {
+  const user = usePage().props.auth.user;
+  const { data, setData, post, processing } = useForm({
+    comment: "",
+  });
 
   const createComment: FormEventHandler = (ev) => {
     ev.preventDefault();
 
-    post(route('comment.store', feature.id), {
+    post(route("comment.store", feature.id), {
       preserveScroll: true,
       preserveState: true,
-      onSuccess: () => setData('comment', '')
-    })
+      onSuccess: () => setData("comment", ""),
+    });
+  };
+
+  if (!can(user, "manage_comments")) {
+    return (
+      <div className="text-center text-gray-600">
+        You don't have permission to leave comments
+      </div>
+    );
   }
 
-
   return (
-    <form onSubmit={createComment} className="flex items-center py-2 rounded-lg bg-gray-50 dark:bg-gray-800 mb-4">
+    <form
+      onSubmit={createComment}
+      className="flex items-center py-2 rounded-lg bg-gray-50 dark:bg-gray-800 mb-4"
+    >
       <TextAreaInput
         rows={1}
         value={data.comment}
-        onChange={e => setData('comment', e.target.value)}
+        onChange={(e) => setData("comment", e.target.value)}
         className="mt-1 block w-full"
         placeholder="Your comment"
       ></TextAreaInput>
