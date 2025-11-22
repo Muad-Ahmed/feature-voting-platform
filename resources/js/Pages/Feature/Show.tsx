08c620eea373f2e949e2a047d21a46b30ec2a14b
Muad-Ahmed <muad8010@gmail.com>
2025-11-22 06:56:39 +0300

    refactor: Integrate upvote/downvote component into Feature Show page
    Cleans up the Feature/Show.tsx component and imports the FeatureUpvoteDownvote component to enable voting functionality on the dedicated feature view page.

--- resources/js/Pages/Feature/Show.tsx
+++ resources/js/Pages/Feature/Show.tsx
@@ -1,9 +1,10 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import {Head} from '@inertiajs/react';
-import {Feature, PaginatedData} from "@/types";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { Feature, PaginatedData } from "@/types";
 import FeatureItem from "@/Components/FeatureItem";
+import FeatureUpvoteDownvote from "@/Components/FeatureUpvoteDownvote";
 
-export default function Show({feature}: {feature: Feature}) {
+export default function Show({ feature }: { feature: Feature }) {
   return (
     <AuthenticatedLayout
       header={
@@ -12,34 +13,12 @@ export default function Show({feature}: {feature: Feature}) {
         </h2>
       }
     >
-      <Head title={'Feature ' + feature.name}/>
+      <Head title={"Feature " + feature.name} />
 
       <div className="mb-4 overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
         <div className="p-6 text-gray-900 dark:text-gray-100 flex gap-8">
-          <div className="flex flex-col items-center">
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg"
-                   viewBox="0 0 24 24"
-                   fill="currentColor" className="size-12">
-                <path fillRule="evenodd"
-                      d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z"
-                      clipRule="evenodd"/>
-              </svg>
-            </button>
-            <span className="text-2xl font-semibold">
-            12
-          </span>
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg"
-                   viewBox="0 0 24 24"
-                   fill="currentColor" className="size-12">
-                <path fillRule="evenodd"
-                      d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
-                      clipRule="evenodd"/>
-              </svg>
+          <FeatureUpvoteDownvote feature={feature} />
 
-            </button>
-          </div>
           <div className="flex-1">
             <h2 className="text-2xl mb-2">{feature.name}</h2>
             <p>{feature.description}</p>
